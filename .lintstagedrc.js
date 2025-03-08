414ab2d104dd41e43a4ed421c4acb94eb4369690
Penn York <markyan@foxmail.com>
2025-03-08 16:11:22 +0800

    chroe: init

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+	// TypeScript, JavaScript files
+	"*.{ts,tsx,js,jsx}": (files) => [
+		"bun typecheck",
+		`bun run format:check ${files.join(" ")}`,
+		`bunx eslint --quiet --fix ${files.join(" ")}`,
+	],
+
+	// Other files
+	"*.{json,mdx}": (files) => [`bun run format:check ${files.join(" ")}`],
+
+	// 明确忽略 tsbuildinfo 文件
+	"!tsconfig.tsbuildinfo": [],
+};
